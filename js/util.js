0caffdfa6bf31c9875d4097d15769277b39c1728
Arthur Maurer <arthimaurer@gmail.com>
2015-08-05 21:21:25 +0200

    Util

--- js/util.js
+++ js/util.js
@@ -1,4 +1,17 @@
+
+'use strict';
+
 function c(m)
 {
 	console.log.apply(console, arguments);
 }
+
+Element.prototype.remove = function ()
+{
+    this.parentElement.removeChild(this);
+};
+
+Number.prototype.clamp = function (min, max)
+{
+    return Math.min(Math.max(this, min), max);
+};
